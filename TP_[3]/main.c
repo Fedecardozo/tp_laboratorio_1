5c1a131d35318573d6bd695730f0e76882545a30
Fede <fedecardozo.a@gmail.com>
2022-06-11 11:12:14 -0300

    Menu

--- TP_[3]/main.c
+++ TP_[3]/main.c
@@ -3,6 +3,9 @@
 #include "LinkedList.h"
 #include "Controller.h"
 #include "Passenger.h"
+#include "utn.h"
+
+#define NAME_ARCHIVO "data.csv"
 
 /****************************************************
     Menu:
@@ -18,14 +21,17 @@
     10. Salir
 *****************************************************/
 
-
+int menu();
 
 int main()
 {
-    int option = 0;
+	setbuf(stdout,NULL);
 
+    int option=0;
     LinkedList* listaPasajeros = ll_newLinkedList();
+
     do{
+    	option = menu();
         switch(option)
         {
             case 1:
@@ -34,4 +40,24 @@ int main()
         }
     }while(option != 10);
     return 0;
+}
+
+int menu()
+{
+	int num=11;
+
+	utn_getNumero(&num,"\n1. Cargar los datos de los pasajeros desde el archivo data.csv (modo texto)."
+			"\n2. Cargar los datos de los pasajeros desde el archivo data.csv (modo binario)."
+			"\n3. Alta de pasajero"
+			"\n4. Modificar datos de pasajero"
+			"\n5. Baja de pasajero"
+			"\n6. Listar pasajeros"
+			"\n7. Ordenar pasajeros"
+			"\n8. Guardar los datos de los pasajeros en el archivo data.csv (modo texto)."
+			"\n9. Guardar los datos de los pasajeros en el archivo data.csv (modo binario)."
+			"\n10. Salir"
+			"\nINGRESE OPCION: "
+			,"\nOPCION INVALIDA.\nIngrese nuevamente: ", 1, 10, 2);
+
+	return num;
 }
